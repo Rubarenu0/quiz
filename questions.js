b916fcf5ef16195ef8841ee8c4490048c65e0d4d
Rubarenu0 <142423853+Rubarenu0@users.noreply.github.com>
2024-03-25 14:21:04 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,61 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What does HTML stands for?",
+        answer: "D. Hyper Text Markup Language",
+        options:[
+            "A. Hyper Type Multi Language",
+            "B. Hyper Text Multi Language",
+            "C. Hyper Type Markup Language",
+            "D. Hyper Text Markup Language"
+
+        ]
+    },
+    {
+        numb: 2,
+        question: "What does CSS stands for?",
+        answer: "A. Cascading Style Sheet",
+        options:[
+            "A. Cascading Style Sheet",
+            "B. Cute Style Sheet",
+            "C. Computer Style Sheet",
+            "D. Cascading Styling Sheet"
+
+        ]
+    },
+    {
+        numb: 3,
+        question: "What is Vue JS?",
+        answer: "B. A Framework",
+        options:[
+            "A. Scripting Language",
+            "B. A Framework",
+            "C. Programming Language",
+            "D. Directives"
+
+        ]
+    },
+    {
+        numb: 4,
+        question: "Who is the father of HTML?",
+        answer: "C. Tim Berners-Lee",
+        options:[
+            "A. Rasmus Lerdorf",
+            "B. Brendan Eich",
+            "C. Tim Berners-Lee",
+            "D. Sergey Brin"
+
+        ]
+    },
+    {
+        numb: 5,
+        question: "Which font format is used in web pages?",
+        answer: "C. WOFF",
+        options:[
+            "A. EOT",
+            "B. WOFF 2.0",
+            "C. WOFF",
+            "D. SVG Fonts"
+        ]
+    }
+];
---
+++ script.js
@@ -0,0 +1,104 @@
+const startBtn=document.querySelector('.start-btn');
+const popupInfo=document.querySelector('.popup');
+const exitBtn=document.querySelector('.exit-btn');
+const main=document.querySelector('.main');
+const continueBtn=document.querySelector('.continue-btn');
+const quizSection=document.querySelector('.quiz-section');
+const quizBox=document.querySelector('.quiz-box');
+startBtn.onclick = () => {
+    popupInfo.classList.add('active');
+    main.classList.add('active');
+}
+exitBtn.onclick = () => {
+    popupInfo.classList.remove('active');
+    main.classList.remove('active');
+}
+continueBtn.onclick = () => {
+    quizSection.classList.add('active');
+    popupInfo.classList.remove('active');
+    main.classList.remove('active');
+    quizBox.classList.add('active');
+
+    showQuestions(0);
+    questionCounter(1);
+    headerScore();
+}
+
+let questionCount=0;
+let questionNumb=1;
+let userScore=0;
+
+const nextBtn=document.querySelector('.next-btn');
+
+nextBtn.onclick = () => {
+    if(questionCount<questions.length-1){
+        questionCount++;
+        showQuestions(questionCount);
+
+        questionNumb++;
+        questionCounter(questionNumb);
+    }
+    else{
+        console.log('Question Completed');
+    }
+    
+}
+const optionList=document.querySelector('.option-list');
+function showQuestions(index)
+{
+    const questionText=document.querySelector('.question-text');
+    questionText.textContent=`${questions[index].numb}.${questions[index].question}`;
+
+    let optionTag=`<div class="option"><span>${questions[index].options[0]}</span></div>
+    <div class="option"><span>${questions[index].options[1]}</span></div>
+    <div class="option"><span>${questions[index].options[2]}</span></div>
+    <div class="option"><span>${questions[index].options[3]}</span></div>`;
+
+    optionList.innerHTML=optionTag;
+
+    const option=document.querySelectorAll('.option');
+    for (let i=0;i<option.length;i++)
+    {
+        option[i].setAttribute('onclick','optionSelected(this)');
+
+    }
+
+}
+
+function optionSelected(answer)
+{
+    let userAnswer=answer.textContent;
+    let correctAnswer=questions[questionCount].answer;
+    let allOptions=optionList.children.length;
+    if(userAnswer==correctAnswer)
+    {
+        answer.classList.add('correct');
+        userScore+=1;
+        headerScore();
+    }
+    else{
+        answer.classList.add('incorrect');
+
+        for(let i=0;i<allOptions;i++)
+    {
+        if(optionList.children[i].textContent==correctAnswer)
+        {
+            optionList.children[i].setAttribute('class','option correct');
+        }
+    }
+    }
+    for(let i=0;i<allOptions;i++)
+    {
+        optionList.children[i].classList.add('disabled');
+    }
+}
+function questionCounter(index){
+    const questionTotal=document.querySelector('.question-total');
+    questionTotal.textContent=`${index} of ${questions.length} Questions`;
+}
+
+function headerScore()
+{
+    const headerScoreText=document.querySelector('.header-score');
+    headerScoreText.textContent=`Score: ${userScore} / ${questions.length}`;
+}
